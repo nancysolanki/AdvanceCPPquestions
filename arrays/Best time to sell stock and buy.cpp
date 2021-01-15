5ece251d08ad5e624ad84cfd9d4f7505e2a0cf61
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-15 17:24:13 +0530

    Update Best time to sell stock and buy.cpp

--- arrays/Best time to sell stock and buy.cpp
+++ arrays/Best time to sell stock and buy.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int max=0;
+        for(int i=1;i<prices.size();i++)
+        {
+            if(prices[i-1]<prices[i])
+            {
+                max=max+prices[i]-prices[i-1];
+            }
+        }
+        return max;
+    }
+};
