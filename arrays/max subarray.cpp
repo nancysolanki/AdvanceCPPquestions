8b77207126ebb9aba2785572e7e335d663962c15
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-15 18:25:05 +0530

    Update max subarray.cpp

--- arrays/max subarray.cpp
+++ arrays/max subarray.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {  //nancy you have to watch kadane algo videos when revising
+        int maxcurr,maxg;
+        maxcurr=maxg=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            maxcurr=max(nums[i],maxcurr+nums[i]);
+            if(maxcurr>maxg)
+            {
+                maxg=maxcurr;
+            }
+        }
+        return maxg;
+        
+    }
+};
