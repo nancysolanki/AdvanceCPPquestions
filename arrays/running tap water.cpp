1173d19b729e1ff7790b39c3b19525fbd9271dda
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-15 17:24:56 +0530

    Update running tap water.cpp

--- arrays/running tap water.cpp
+++ arrays/running tap water.cpp
@@ -1,1 +1,31 @@
 
+class Solution {
+public:
+    int trap(vector<int>& height)
+    {  
+        int n=height.size();
+        if(n==0)
+        {
+            return 0;
+        }
+        int total=0;
+        int l[n];
+        l[0]=height[0];
+        int r[n];
+        r[n-1]=height[n-1];
+      
+        for(int i=1;i<n;i++)
+        {
+              l[i]=max(l[i-1],height[i]);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+               r[i]=max(height[i],r[i+1]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            total =total+min(l[i],r[i])-height[i];
+        }
+        return total;
+    }
+};
