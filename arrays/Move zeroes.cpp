3e9afdd2ec7de66bf423fe243931434b23f7d073
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-16 16:35:08 +0530

    Update Move zeroes.cpp

--- arrays/Move zeroes.cpp
+++ arrays/Move zeroes.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        //This is my code i am smert
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (;j < nums.size(); j++) {
+            nums[j] = 0;
+        }
+    }
+};
