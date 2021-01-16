4166c2aa34fe69a210a7ee2c5837cb9522ab7669
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-16 16:51:38 +0530

    Update Single Number .cpp

--- bit manipulation/Single Number .cpp
+++ bit manipulation/Single Number .cpp
@@ -1,1 +1,15 @@
+// I have used property of XOR a^a=0 and a^0=a
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+         int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans=ans^nums[i];
+        }
+        return ans;
+    }
+};
 
