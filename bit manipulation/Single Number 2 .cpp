4a63f24ba18d2e24b8df24748ae255469499cdbb
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-16 17:28:21 +0530

    Update Single Number 2 .cpp

--- bit manipulation/Single Number 2 .cpp
+++ bit manipulation/Single Number 2 .cpp
@@ -1,1 +1,28 @@
-
+//https://www.youtube.com/watch?v=cOFAmaMBVps&t=44s
+// you can have a basic idea on how to approach this problem through this YT link
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {  int res=0;
+      vector<int>f(32,0);
+    
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<32;j++)
+            {
+                if(nums[i]&(1<<j))
+                {
+                    f[j]++;
+                }
+            }
+        }
+        for(int i=0;i<32;i++)
+        {
+            if(f[i]%3==1)
+            {
+                res=res+(1<<i);
+            }
+        }
+     return res;
+    }
+};
