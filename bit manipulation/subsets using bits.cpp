1671616eac99ff24d496873d9b82344d44caeb48
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-16 19:28:16 +0530

    Create subsets using bits.cpp

---
+++ bit manipulation/subsets using bits.cpp
@@ -0,0 +1,18 @@
+//Given an integer array nums, return all possible subsets
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                }
+            }
+        }
+        return subs;
+    }
+};
+ 
