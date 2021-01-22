7ef2dcfab877572bb4d689fea049d958b66a1547
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-22 22:36:55 +0530

    Update Prime.cpp

--- Number Theory/Prime.cpp
+++ Number Theory/Prime.cpp
@@ -1,1 +1,31 @@
 
+vector<int> Solution::sieve(int A) 
+{
+    bool prime[A+1];
+    vector<int> ans;
+    for(int i=0;i<=A;i++)
+    {
+        prime[i]=true;
+    }
+    prime[0]=false;
+    prime[1]= false;
+    for(int i=2;i<=A;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int j=i+i;j<=A;j=j+i)
+            {
+                prime[j]=false;
+            }
+        }
+        
+    }
+    for(int i=2;i<=A;i++)
+    {
+        if(prime[i]==true)
+        {
+            ans.push_back(i);
+        }
+    }
+    return ans;
+}
