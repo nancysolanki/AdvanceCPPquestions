e1029ff7b1585ec4c8c48b9b464f4cda2bde41e6
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-23 18:34:47 +0530

    Update Palindrome.cpp

--- Number Theory/Palindrome.cpp
+++ Number Theory/Palindrome.cpp
@@ -1,1 +1,24 @@
+//YT LINK- https://www.youtube.com/watch?v=UPdSViixmDs
 
+bool isPalindrome(int x) 
+{
+
+
+if( x < 0 ) return false;
+    
+    int result = 0;
+    int num = x;
+    
+    while( num > 0 )
+    {
+        result = result * 10 + num % 10;
+        
+        if( result > x ) return false;
+        
+        num = num / 10;
+    }
+    
+    if( result == x ) return true;
+    
+    else return false;
+}
