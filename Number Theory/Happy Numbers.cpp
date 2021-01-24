551ce881dab863cd210ed8475ea4f9d589d5ad42
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-24 19:36:40 +0530

    Update Happy Numbers.cpp

--- Number Theory/Happy Numbers.cpp
+++ Number Theory/Happy Numbers.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    bool isHappy(int n) {
+while (true) {
+        
+        long sum = 0;
+        
+        while (n != 0) {
+            
+            int lastDigit = n % 10;
+            n /= 10;
+            sum += lastDigit * lastDigit;
+        }
+        
+        if (1 <= sum && sum <= 9) {
+            
+            if (sum == 1 || sum == 7) return true;
+            else return false;
+        }
+        else n = sum;
+    }
+  }
+};
