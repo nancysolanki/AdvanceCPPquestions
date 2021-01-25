7ee8cba7f40e9183f30258c841c6140cb6f0184d
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-25 18:19:20 +0530

    Update Max point on a line.cpp

--- Number Theory/Max point on a line.cpp
+++ Number Theory/Max point on a line.cpp
@@ -1,1 +1,26 @@
 
+public:
+    int maxPoints(vector<Point>& points) 
+    {
+        if(points.size()<=2) return points.size();
+        int res=0;
+        for(int i=0;i<points.size()-1;i++) {
+            int numVertical=1,local=1,duplicate=0;
+            unordered_map<double,int> map;
+            for(int j=i+1;j<points.size();j++) 
+                if(points[i].x==points[j].x) // special cases
+                    if(points[i].y==points[j].y) // duplicate 
+                        duplicate++;
+                    else // vertical
+                        numVertical++;
+                else {
+                    double slope=(points[i].y-points[j].y)*1.0/(points[i].x-points[j].x);
+                    map[slope]==0?map[slope]=2:map[slope]++;
+                    local=max(local,map[slope]);
+                }
+            local=max(local+duplicate,numVertical+duplicate);
+            res=max(res,local);
+        }
+        return res;
+    }
+}
