f72e9b44b85d1684c230ca99a5b51969cf02ed29
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-02-01 19:39:35 +0530

    Update Search a 2d matrix.cpp

--- Search/Search a 2d matrix.cpp
+++ Search/Search a 2d matrix.cpp
@@ -1,1 +1,29 @@
+//using binary search approach 
 
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+          int row = matrix.size();
+        int col = matrix[0].size();
+        int i = row-1,j =0;
+        while(i>=0&&j<=col-1)
+        {
+            if(target>matrix[i][j])
+            {
+                j++;
+            }
+            else if(target<matrix[i][j])
+            {
+                i--;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+};
