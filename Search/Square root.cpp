79c7106a2250ffd9ae5c46c6510667ea131904c2
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-02-05 21:00:43 +0530

    Update Square root.cpp

--- Search/Square root.cpp
+++ Search/Square root.cpp
@@ -1,1 +1,17 @@
-
+//using simple maths i.e 2*2=4 , I coded this approach
+class Solution {
+public:
+    int mySqrt(int x) 
+    {  long long s=0, e=x, ans, mid;   
+        while(s<=e){             
+            mid=(s+e)/2;
+            if(mid*mid==x) return mid;     
+            else if(mid*mid<x){             
+                s=mid+1;       
+                ans=mid;         
+            }
+            else e=mid-1;        
+        }
+        return ans; 
+    }
+};
