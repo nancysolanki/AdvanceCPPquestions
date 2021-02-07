aef29bdacdd3442b8ef8962c700e74d0db746366
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-02-07 22:15:33 +0530

    Update K th permutation sequence.cpp

--- Backtracking/K th permutation sequence.cpp
+++ Backtracking/K th permutation sequence.cpp
@@ -1,1 +1,17 @@
+/*We can use next_permutation that modifies a string so that it stores lexicographically next permutation.
+*/
+string Solution::getPermutation(int A, int B) 
+{  
+    vector<int> v;
+for(int i=1;i<=A;i++)v.push_back(i);
 
+do{
+    B--;
+}while(next_permutation(v.begin(),v.end()) && B!=1);
+
+string s="";
+for(int i: v){
+    s = s +  to_string(i); 
+}
+return s;
+}
