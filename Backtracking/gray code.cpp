2a10cf03b5c2962c607b5938709faf7e77975dec
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-02-09 22:47:48 +0530

    Update gray code.cpp

--- Backtracking/gray code.cpp
+++ Backtracking/gray code.cpp
@@ -1,5 +1,11 @@
 /*XOR works by setting the bits which are set in either of one of the given numbers (0 ^ 1 = 1, 1 ^ 0 = 1) and finally taking out the common bits 
-present in both numbers (1 ^ 1 = 0).*/
+present in both numbers (1 ^ 1 = 0).
+ 
+Given decimal number n with bits positions I (I+1) (I+2) … numbered from left to right
+Its Gray code is I (I^(I+1)) ((I+1)^(I+2)) …
+Notice each bit is xoring with previous bit
+(so if we take a number and right shift it we will bring previous bit position for given bit in current number)
+Gray code equivalent hence is for n = n^(n/2)*/
 vector Solution::grayCode(int A) {
 vector ans;
 long long x = pow(2 , A) , i;
