0920dc0bccc7918432282753f07b322c3933b6e1
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-02-12 22:46:15 +0530

    Update pallindrome partitioning.cpp

--- Backtracking/pallindrome partitioning.cpp
+++ Backtracking/pallindrome partitioning.cpp
@@ -1,1 +1,30 @@
+/* Iteratively generate all possible substrings beginning at \text{start}start index. The \text{end}end index increments from \text{start}start till the end of the string.
+
+For each of the substring generated, check if it is a palindrome.
+
+If the substring is a palindrome, the substring is a potential candidate. Add substring to the \text{currentList}currentList and perform a depth-first search on the remaining
+substring. If current substring ends at index \text{end}end, \text{end}+1end+1 becomes the \text{start}start index for the next recursive call.
+
+Backtrack if \text{start}start index is greater than or equal to the string length and add the \text{currentList}currentList to the result.*/
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> currentList;
+        dfs(result, s, 0, currentList);
+        return result;
+    }
+
+    void dfs(vector<vector<string>> &result, string &s, int start, vector<string> &currentList) {
+        if (start >= s.length()) result.push_back(currentList);
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                // add current substring in the currentList
+                currentList.push_back(s.substr(start, end - start + 1));
+                dfs(result, s, end + 1, currentList);
+                // backtrack and remove the current substring from currentList
+                currentList.pop_back();
+            }
+        }
+    }
 
