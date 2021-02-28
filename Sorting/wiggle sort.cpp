9bf10c66b5326078ae8e12d18e48f2a5181dcad4
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-02-28 19:32:20 +0530

    Update wiggle sort.cpp

--- Sorting/wiggle sort.cpp
+++ Sorting/wiggle sort.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n);
+        sort(nums.begin(),nums.end());
+        int j=n-1;
+        for(int i=1;i<n;i=i+2)
+        {
+            ans[i]=nums[j];
+            j--;
+        }
+        for(int i=0;i<n;i=i+2)
+        {
+            ans[i]=nums[j];
+            j--;
+        }
+        nums=ans;
+
+        
+    }
+};
