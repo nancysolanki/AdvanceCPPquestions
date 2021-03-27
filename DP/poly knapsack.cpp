474ff147f0323af4b59e8733f643963adbb597ad
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-03-27 23:54:46 +0530

    Update poly knapsack.cpp

--- DP/poly knapsack.cpp
+++ DP/poly knapsack.cpp
@@ -1,1 +1,25 @@
 
+int solve(vector<int>& weights, vector<int>& values, int capacity) 
+{
+   int n=values.size();
+   if(n==0)
+   {
+       return 0;
+   }
+  int dp[capacity+1];
+  for(int i=0;i<=capacity;i++)
+  {
+      dp[i]=0;
+  }
+  for(int i=0;i<=capacity;i++)
+  {
+      for(int j=0;j<n;j++)
+      {
+          if(weights[j]<=i)
+          {
+              dp[i]=max(dp[i],dp[i-weights[j]]+values[j]);
+          }
+
+      }
+  }return dp[capacity];
+}
