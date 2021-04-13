569255124dd6a86dd4a2d135259ea4be62751129
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-04-13 19:49:38 +0530

    Update Substring Pallindrome.cpp

--- DP/Substring Pallindrome.cpp
+++ DP/Substring Pallindrome.cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int count = 0, n = s.size();
+        bool dp[n][n];
+        memset(dp, false, sizeof dp);
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j<n; j++){
+                if(i == j) dp[i][j] = true;
+                else if(i+1 == j && s[i] == s[j]) dp[i][j] = true;
+                else{
+                    dp[i][j] = s[i]==s[j] && dp[i+1][j-1];
+                }
+                if(dp[i][j]) count++;
+            }
+        }
+        return count;
+    }
+};
