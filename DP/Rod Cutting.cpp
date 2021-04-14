850e54d7c32dc94ba2d218c2cb1dae12420a3e79
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-04-14 20:05:11 +0530

    Update Rod Cutting.cpp

--- DP/Rod Cutting.cpp
+++ DP/Rod Cutting.cpp
@@ -1,1 +1,16 @@
 
+int max(int a, int b) { return (a > b)? a : b;}
+ 
+
+int cutRod(int price[], int n)
+{
+   if (n <= 0)
+     return 0;
+   int max_val = INT_MIN;
+ 
+   for (int i = 0; i<n; i++)
+         max_val = max(max_val, price[i] + cutRod(price, n-i-1));
+ 
+   return max_val;
+}
+ 
