9a93c36d5e8483615856324aba479f9e497b33f2
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-05-30 17:24:32 +0530

    Update BFS.cpp

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -1,1 +1,28 @@
 
+void bfs(vector<int>adj[], int v , int s)  //s is the starting index
+{
+  bool visited[v+1];
+  for(int i=0;i<v;i++)
+  {
+    visited[i]=false;
+  }
+  queue<int>q;
+  visited[s]=true;
+  q.push(s);
+  while(!q.empty())
+  {
+    int curr=q.front();
+    q.pop();
+    cout<<curr<<" ";
+    
+    for(int i:adj[curr])
+    {
+      if(visited[i]==false)
+      {
+        visited[i]=true;
+        q.push(i);
+      }
+    }
+  }
+  
+}
