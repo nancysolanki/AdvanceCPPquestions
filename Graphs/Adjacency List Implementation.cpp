69a9324a8dc7727dc41cba79ce534fae48e6c622
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-05-30 17:12:52 +0530

    Update Adjacency List Implementation.cpp

--- Graphs/Adjacency List Implementation.cpp
+++ Graphs/Adjacency List Implementation.cpp
@@ -1,1 +1,19 @@
-
+void addedge(vector<int>adj[], int u , int v)
+{
+  adj[u].push_back[v];
+  adj[v].push_back[u];
+}
+int main()
+{
+  int V=4;
+  vector<int>adj[V];
+  addedge(adj,0,1);
+  addedge(adj,0,2);
+  addedge(adj,1,2);
+  addedge(adj,1,3);
+  return 0;
+}
+//0--->1,2
+//1--->0,2,3
+//2--->0,1
+//3--->1
