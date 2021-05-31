fa651c60920df8ddfde005300431ccb70d624d6b
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-05-31 10:00:09 +0530

    Update detect cycle in undirected graph.cpp

--- Graphs/detect cycle in undirected graph.cpp
+++ Graphs/detect cycle in undirected graph.cpp
@@ -1,1 +1,61 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+class Solution 
+{
+    public:
+bool helper(int V, vector<int>adj[],int si ,bool visited[])
+{
+    vector<int>p(V,-1);
+    queue<int>q;
+   
+    visited[si]=true;
+     q.push(si);
+    while(!q.empty())
+    {
+       int curr=q.front();
+       q.pop();
+       for(auto j : adj[curr])
+       {
+           if(!visited[j])
+           {
+               visited[j]=true;
+               q.push(j);
+               p[j]=curr;
+           }
+           else if(p[curr]!=j)
+           {
+               return true;
+           }
+           
+       }
+    }
+    return false;
+    
+    
+}
+
+	bool isCycle(int V, vector<int>adj[])
+	{
+	    vector<int>p;
+	    bool visited[V];
+	    for(int i=0;i<V;i++)
+	    {
+	        visited[i]=false;
+	    }
+	    for(int i=0;i<V;i++)
+	    {
+	        if(visited[i]==false and  helper(V,adj,i,visited))
+	        {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+};
 
