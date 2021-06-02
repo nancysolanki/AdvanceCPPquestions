37b574e4dd8710f548e0c7a8a7640c6df474edcf
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-06-02 09:20:01 +0530

    Update Find Centre of star graph.cpp

--- Graphs/Find Centre of star graph.cpp
+++ Graphs/Find Centre of star graph.cpp
@@ -1,1 +1,14 @@
+//There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges
+//that connect the center node with every other node.
+//You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
 
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) 
+    {   if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1])
+        {
+        return edges[0][0];
+       }
+     return edges[0][1];
+    }
+};
