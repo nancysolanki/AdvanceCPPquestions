1e2191ce6732d2fa401aa1d8e022ff37888fe067
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2025-09-14 15:57:40 +0530

    Create CycleDetection.cpp

---
+++ LinkedList /CycleDetection.cpp
@@ -0,0 +1,31 @@
+//Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
